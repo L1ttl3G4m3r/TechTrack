b3a8b1236e48901b1e7a5c3140596a4c9063d55e
L1ttl3G4m3r <kaylee.hoek@hva.nl>
2025-11-11 16:47:55 +0100

    2 charts die nog niks zeggen

--- PokémonTCG/PokémonTCG/src/lib/components/BinderOverlay.svelte
+++ PokémonTCG/PokémonTCG/src/lib/components/BinderOverlay.svelte
@@ -1,11 +1,12 @@
 <script>
   import * as d3 from "d3";
-  export let cards = [];
+  export let cards = []; // data uit +page.svelte
   export let onClose;
 
   let svgContainer;
+  let chartType = 'line'; // 'line' of 'bar'
 
-  $: if (cards.length && svgContainer) renderChart();
+  $: if (svgContainer && cards.length) renderChart();
 
   function renderChart() {
     const svg = d3.select(svgContainer);
@@ -15,43 +16,82 @@
     const height = 400;
     svg.attr("width", width).attr("height", height);
 
+    if (chartType === 'line') renderLineChart(svg, cards, width, height);
+    else renderBarChart(svg, cards, width, height);
+  }
+
+  function renderLineChart(svg, data, width, height) {
     const x = d3.scaleLinear()
-      .domain([0, d3.max(cards, d => d.price)])
+      .domain([0, data.length - 1])
       .range([50, width - 50]);
 
     const y = d3.scaleLinear()
-      .domain([0, cards.length - 1])
-      .range([50, height - 50]);
+      .domain([0, d3.max(data, d => d.price)])
+      .range([height - 50, 50]);
 
-    const r = d3.scaleSqrt()
-      .domain([0, d3.max(cards, d => d.price)])
-      .range([10, 40]);
+    const line = d3.line()
+      .x((d,i) => x(i))
+      .y(d => y(d.price));
+
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "#ffcb05")
+      .attr("stroke-width", 3)
+      .attr("d", line);
 
     svg.selectAll("circle")
-      .data(cards)
+      .data(data)
       .enter()
       .append("circle")
-      .attr("cx", (d, i) => x(d.price))
-      .attr("cy", (d, i) => y(i))
-      .attr("r", d => r(d.price))
-      .attr("fill", "#ffcb05")
-      .attr("stroke", "#3b4cca")
-      .attr("stroke-width", 2);
+      .attr("cx", (d,i) => x(i))
+      .attr("cy", d => y(d.price))
+      .attr("r", 5)
+      .attr("fill", "#3b4cca");
+  }
+
+  function renderBarChart(svg, data, width, height) {
+    const x = d3.scaleBand()
+      .domain(data.map(d => d.name))
+      .range([50, width - 50])
+      .padding(0.2);
+
+    const y = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.price)])
+      .range([height - 50, 50]);
+
+    svg.selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", d => x(d.name))
+      .attr("y", d => y(d.price))
+      .attr("width", x.bandwidth())
+      .attr("height", d => height - 50 - y(d.price))
+      .attr("fill", "#ffcb05");
 
     svg.selectAll("text")
-      .data(cards)
+      .data(data)
       .enter()
       .append("text")
-      .text(d => d.name)
-      .attr("x", (d, i) => x(d.price))
-      .attr("y", (d, i) => y(i) - r(d.price) - 5)
+      .text(d => d.price)
+      .attr("x", d => x(d.name) + x.bandwidth()/2)
+      .attr("y", d => y(d.price) - 5)
       .attr("text-anchor", "middle")
-      .attr("font-size", "10px")
-      .attr("fill", "white");
+      .attr("fill", "white")
+      .attr("font-size", "12px");
+  }
+
+  function toggleChart() {
+    chartType = chartType === 'line' ? 'bar' : 'line';
+    renderChart();
   }
 </script>
 
 <div class="overlay">
   <button class="close-overlay" on:click={onClose}>← Terug</button>
+  <button class="overlay-btn" on:click={toggleChart} style="position:absolute; top:1rem; right:1rem;">
+    {chartType === 'line' ? 'Bar Chart' : 'Line Chart'}
+  </button>
   <svg bind:this={svgContainer}></svg>
 </div>
